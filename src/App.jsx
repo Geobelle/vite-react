1bfd22206db2c11064d6b6c1032941696787cc6d
Georgina Riebelle <135925781+Geobelle@users.noreply.github.com>
2024-11-14 16:14:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from './components/Nav.jsx'
+import Main from './components/MainComp.jsx'
+
+function App() {
+
+  return (
+    <div className="container">
+      <NavBar/>
+      <Main/>
+    </div>
+   
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+ 
+function NavBar() {
+
+    return (
+      <div className="nav">
+        <img src='react-logo-1-removebg-preview.png'/>
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>
+      </div>
+    )
+  }
+  export default NavBar
